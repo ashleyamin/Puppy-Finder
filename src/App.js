6c0165aa20b4832fcb1f58f574762b76126c86e7
kjerrya <lifthz@gmail.com>
2018-01-30 16:43:42 -0500

    Appjs ready to import and render commented out incomplete components for now

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React {Component} from 'react';
+import {BrowserRouter as Router} from 'react-dom';
+import './App.css';
+
+import Header from './components/Header';
+// import Footer from './components/Footer';
+
+
+class App extends Component {
+    render() {
+        return (
+            <Header />
+            <Router>
+  //              <Route exact "/Home" {Search} />
+  //              <Route exact "/breed" {Results} />
+  //              <Route exact "/breed/:id" {SingeResult} />
+  //              <Route exact "/favorites" {Favorites} />
+  //              <Route exact "/about" {SingleFavorite} />
+  //              <Route exact "/about" {About} />
+
+            </Router>
+  //        <Footer />
+
+        )
+
+    }
+
+}
+
+export default App;

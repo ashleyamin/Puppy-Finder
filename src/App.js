c5c9cb14e9b3faabcae6b0c5eda909585fa380ac
kjerrya <lifthz@gmail.com>
2018-01-30 16:57:04 -0500

    Updated Appjs to match current files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React {Component} from 'react';
 import {BrowserRouter as Router} from 'react-dom';
-import './App.css';
+import './app.css';
+
 
 import Header from './components/Header';
 // import Footer from './components/Footer';
@@ -9,17 +10,20 @@ import Header from './components/Header';
 class App extends Component {
     render() {
         return (
+            <div className="App">
             <Header />
             <Router>
-  //              <Route exact "/Home" {Search} />
-  //              <Route exact "/breed" {Results} />
-  //              <Route exact "/breed/:id" {SingeResult} />
-  //              <Route exact "/favorites" {Favorites} />
-  //              <Route exact "/about" {SingleFavorite} />
-  //              <Route exact "/about" {About} />
+  /*              <Route exact "/Home" {Search} />
+                <Route exact "/breed" {Results} />
+                <Route exact "/breed/:id" {SingeResult} />
+                <Route exact "/favorites" {Favorites} />
+                <Route exact "/about" {SingleFavorite} />
+                <Route exact "/about" {About} /> */
 
             </Router>
-  //        <Footer />
+  /*        <Footer /> */
+            </div>
+
 
         )
 

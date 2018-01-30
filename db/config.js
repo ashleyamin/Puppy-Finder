43c13f24ecda9e4540edb003a504f9ba3b5d3ad3
debbie chong <giyoungchong1980@gmail.com>
2018-01-30 16:42:11 -0500

    add config.js fie and migrations file

--- db/config.js
+++ db/config.js
@@ -1,1 +1,24 @@
 //this is the db config file
+const options = {
+  query: (e) => {
+    console.log(e.query)
+  }
+}
+
+const pgp = require('pg-promise')(options);
+
+const setDataBase = () => {
+  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV){
+    return pgp({
+      database: 'puppy_dev',
+      port: 5432,
+      host: 'localhost'
+    })
+  }
+  else if (process.env.NODE_ENV === 'production'){
+    return pgp(process.env.DATABASE_URL)
+  }
+}
+
+const db = setDataBase();
+module.exports = db;

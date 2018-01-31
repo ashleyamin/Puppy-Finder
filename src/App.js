490c26d6bb181d85f543b9b4b27fea7a7dd0f288
kjerrya <lifthz@gmail.com>
2018-01-31 11:37:37 -0500

    Appjs updated with all components and corrected syntax and dependencies.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React {Component} from 'react';
-import {BrowserRouter as Router} from 'react-dom';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './app.css';
 
+import Search from './components/Search';
+import Results from './component/Results';
+import SingleResult from './component/SingleResult';
+import Favorites from './components/Favorites';
+import SingleFavorite from './components/SingleFavorite';
+import About from './components/About';
 
 import Header from './components/Header';
-// import Footer from './components/Footer';
-
+import Footer from './components/Footer';
 
 class App extends Component {
+
     render() {
         return (
             <div className="App">
-            <Header />
-            <Router>
-  /*              <Route exact "/Home" {Search} />
-                <Route exact "/breed" {Results} />
-                <Route exact "/breed/:id" {SingeResult} />
-                <Route exact "/favorites" {Favorites} />
-                <Route exact "/about" {SingleFavorite} />
-                <Route exact "/about" {About} /> */
+                <Header />
+                <Router>
+                    <Route exact path="/" {Search} />
+                    <Route exact path="/breed" {Results} />
+                    <Route exact path="/breed/:id" {SingleResult} />
+                    <Route exact path="/favorites" {Favorites} />
+                    <Route exact path="/about" {SingleFavorite} />
+                    <Route exact path="/about" {About} />
 
-            </Router>
-  /*        <Footer /> */
+                </Router>
+                <Footer />
             </div>
 
 

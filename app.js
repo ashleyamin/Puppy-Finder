5162dacaa0dd89ea63528f4a4995b7df86a78b61
Joshua Russell <jkrussell756@gmail.com>
2018-01-31 11:23:11 -0500

    some typos fixed

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', (req,res) => {
 
 //API route
 app.use('/api/puppy', require('./routes/puppy-routes'));
-app.use('/api/opinions', require('./routes/opinions-routes'));
+app.use('/api/opinions', require('./routes/opinion-routes'));
 app.use('/api/puppyfinder', require('./routes/puppy-finder-routes'));
 
 // Setting up the port

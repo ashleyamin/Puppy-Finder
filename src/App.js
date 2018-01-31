bcaaf06d1e03f199ac2696049de682182c21388c
kjerrya <lifthz@gmail.com>
2018-01-31 11:36:14 -0500

    Appjs will change swill be in Appjs branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './app.css';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 
 class App extends Component {
 
@@ -12,12 +13,12 @@ class App extends Component {
             <div className="App">
             <Header />
             <Router>
-  /*            <Route exact "/Home" {Search} />
-                <Route exact "/breed" {Results} />
-                <Route exact "/breed/:id" {SingeResult} />
-                <Route exact "/favorites" {Favorites} />
-                <Route exact "/about" {SingleFavorite} />
-                <Route exact "/about" {About} /> */
+                <Route exact path="/Home" {Search} />
+                <Route exact path="/breed" {Results} />
+                <Route exact path="/breed/:id" {SingleResult} />
+                <Route exact path="/favorites" {Favorites} />
+                <Route exact path="/about" {SingleFavorite} />
+                <Route exact path="/about" {About} />
 
             </Router>
             <Footer />

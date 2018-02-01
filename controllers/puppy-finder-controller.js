e9ba3e64f694bfcc1e4b9b8f3d0f13ba89871d83
Joshua Russell <jkrussell756@gmail.com>
2018-02-01 10:48:43 -0500

    puppy finder controller fixed

--- controllers/puppy-finder-controller.js
+++ controllers/puppy-finder-controller.js
@@ -1,5 +1,5 @@
 // Create the puppy finder controller object
-const puppFinderController = {};
+const puppyFinderController = {};
 
 // Make the axios call to puppy finder API
 puppyFinderController.search = (req,res) => {
@@ -8,12 +8,14 @@ puppyFinderController.search = (req,res) => {
     url: (`https://api.petfinder.com/pet.find?key=2a7149beb4237dc3dc4a3123d808f533&breed=${req.body.breed}&location=${req.body.zipcode}&count=2&format=json`),
   })
   .then(allTheDogs => {
-    message: 'successfully made API call',
-    data: allTheDogs.data.petfinder.pets
+    res.json({
+      message: 'successfully made API call',
+      data: allTheDogs.data.data[0].petfinder.pets.pet
+    })
   })
   .catch(err => {
     console.log('inside puppy finder API call error', err);
   });
 };
 
-module.exports = puppFinderController;
+module.exports = puppyFinderController;

54cd25e05b24f952c91975372326f0d0e6998987
debbie chong <giyoungchong1980@gmail.com>
2018-02-01 12:08:37 -0500

    add breed column

--- db/migrations/add-puppies-table.sql
+++ db/migrations/add-puppies-table.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS puppies;
 CREATE TABLE puppies(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
+  breed VARCHAR(255),
   photourl VARCHAR(255),
   sex VARCHAR(255),
   description VARCHAR(255),

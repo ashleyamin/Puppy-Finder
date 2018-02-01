e0a1c6f1a983f301f91c8d3c8ab9879c8bf4d952
Joshua Russell <jkrussell756@gmail.com>
2018-02-01 12:19:17 -0500

    Search finished, ready for testing. Only lacking photos.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,72 +1,106 @@
+// import things
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import Results from './components/Results';
 
+// Instantiate the Search class
 class Search extends Component {
   constructor() {
     super();
     this.state = {
       breed: null,
       zipcode: null,
-      redirect: false,
+      puppiesLoaded: false,
+      puppyData: null,
     };
     // bind the functions
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.newSearch = this.newSearch.bind(this);
   }
   // render each breed option with a photo. placeholder right now.
-  renderBreeds() {
+  renderSearch() {
     return(
-      <div className="wrapper">
+      <div className="searchform">
+      {/* Name and photo of each of the breeds you can search for */}
+        <div className="wrapper">
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Poodle" />
+            <p>Poodle</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Beagle" />
+            <p>Beagle</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Dalmatian" />
+            <p>Dalmatian</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Miniature Schnauzer" />
+            <p>Miniature Schnauzer</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Husky" />
+            <p>Husky</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Labrador Retriever" />
+            <p>Labrador Retriever</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Great Dane" />
+            <p>Great Dane</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="German Shepherd" />
+            <p>German Shepherd</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Chihuahua" />
+            <p>Chihuahua</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Golden Retriever" />
+            <p>Golden Retriever</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Corgi" />
+            <p>Corgi</p>
           </div>
           <div className="breeds">
-            <img src="#" alt="breed name" />
-            <p>BREED NAME</p>
+            <img src="#" alt="Shiba Inu" />
+            <p>Shiba Inu</p>
           </div>
         </div>
-      )
+        {/* Form that submits the search parameters to the axios call in this.handleSubmit */}
+        <form onSubmit={this.handleSubmit}>
+          <label>Select a breed!</label>
+          <select name="breed" value={this.state.breed} onChange={this.handleChange}>
+            <option value="Poodle">Poodle</option>
+            <option value="Beagle">Beagle</option>
+            <option value="Dalmatian">Dalmatian</option>
+            <option value="Miniature Schnauzer">Miniature Schnauzer</option>
+            <option value="Husky">Husky</option>
+            <option value="Labrador Retriever">Labrador Retriever</option>
+            <option value="Great Dane">Great Dane</option>
+            <option value="German Shepherd Dog">German Shepherd</option>
+            <option value="Chihuahua">Chihuahua</option>
+            <option value="Golden Retriever">Golden Retriever</option>
+            <option value="Corgi">Corgi</option>
+            <option value="Shiba Inu">Shiba Inu</option>
+          </select>
+          <input
+            type="text"
+            name="zipcode"
+            placeholder="Zip Code"
+            value={this.state.zipcode}
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="SEARCH HOOMAN" />
+        </form>
+      </div>
+    )
   }
   // Changes state based on what the user inputs into the form
   handleChange(event) {
@@ -79,47 +113,47 @@ class Search extends Component {
   // changes the redirect state so the component knows to redirect to the results page
   handleSubmit(event) {
     event.preventDefault();
-    this.setState({
-      redirect: true,
+    // Post to the puppyfinder controller, which will make a get request to the puppy finder API
+    // Sends the breed and zipcode to the controller
+    axios({
+      method: 'POST',
+      url: ('/api/puppyfinder'),
+      data: {
+        breed: this.state.breed,
+        zipcode: this.state.zipcode,
+      },
+    })
+    // Gets back the puppy data and sticks it into
+    .then(puppyData => {
+      this.setState({
+        puppiesLoaded: true,
+        puppyData: puppyData,
+      })
+    })
+    .catch(err => {
+      console.log('puppyfinder call error', err);
     })
   }
-  //
+  // Changes state back to what it is when the page loads,
+  newSearch() {
+    this.setState({
+      puppiesLoaded: false,
+      puppyData: null,
+    });
+  }
   render() {
-    // redirects to the results page if the form has been submitted
-    if (this.state.redirect) {
-      return(
-        <Redirect to="/breed" />
-        )
-    }
     // renders the page normally
     return(
       <div className="search">
-        {this.renderBreeds}
-        <form onSubmit={this.handleSubmit}>
-          <label>Select a breed!</label>
-          <select name="breed" value={this.state.breed} onChange={this.handleChange}>
-            <option value="1">1</option>
-            <option value="1">2</option>
-            <option value="1">3</option>
-            <option value="1">4</option>
-            <option value="1">5</option>
-            <option value="1">6</option>
-            <option value="1">7</option>
-            <option value="1">8</option>
-            <option value="1">9</option>
-            <option value="1">10</option>
-            <option value="1">11</option>
-            <option value="1">12</option>
-          </select>
-          <input
-                type="text"
-                name="zipcode"
-                placeholder="Zip Code"
-                value={this.state.zipcode}
-                onChange={this.handleChange}
-          />
-          <input type="submit" value="SEARCH HOOMAN" />
-        </form>
+        {/* Conditional rendering. If we loaded data from the puppy finder API the Results component will render.
+            We're passing down the data we got from the back-end as props for results to render.
+            If there's no data from the API the search form will render.
+            The button has an onCLick event to have a new search by resetting state */}
+        {(this.state.puppiesLoaded) ?
+        (<Results puppies={this.state.puppyData} />,
+        <button onClick={this.newSearch}>New Search</button>) :
+        this.renderSearch }
+      }
       </div>
       )
   }
